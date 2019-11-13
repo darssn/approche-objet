14d54d8c033d0364f45f9663b74ce73d71150e45
diginamic <diginamic@192.168.210.45>
2019-11-13 14:14:30 +0100

    TP1 Encapsulation

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,18 @@
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Compte c = new Compte(1,10);
+		
+		
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,19 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	
+	int num ;
+	int solde;
+	
+	
+	public Compte(int num , int solde){
+		
+		this.num = num;
+		this.solde = solde;
+		
+		
+	}
+	
+	
+}
