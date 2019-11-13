b95e437f90c98bc5bcd81d6d26c521954ef55159
diginamic <diginamic@192.168.210.45>
2019-11-13 13:58:02 +0100

    TP04

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		
+		
+
+		String nombre = "123";
+
+		int nb = Integer.parseInt(nombre);
+		
+		int a = 1;
+		int b = 2;
+		
+		
+		System.out.println(Integer.max(a, b));
+		
+		
+
+	}
+}
