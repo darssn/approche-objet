0ce2768ce308efd975edac66014fdc475d240e83
diginamic <diginamic@192.168.210.45>
2019-11-13 11:20:26 +0100

    v1.0

---
+++ src/main/java/entites/AdressePostale.java
@@ -0,0 +1,10 @@
+package entites;
+
+public class AdressePostale {
+	
+	int numRue;
+	String libelle;
+	int cp;
+	String ville;
+
+}
---
+++ src/main/java/essais/TestAdressePostale.java
@@ -0,0 +1,15 @@
+package essais;
+import entites.AdressePostale;
+
+public class TestAdressePostale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AdressePostale a = new AdressePostale() ;
+		
+		AdressePostale b = new AdressePostale() ;
+		
+	}
+
+}
